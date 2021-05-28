e2dbe7589106c073e158452ff7e07e53d2b73c9a
berhane tendai cole <71781247+berhanecole@users.noreply.github.com>
2021-05-28 12:37:47 -0500

    updated webpack configuration;

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  module: {
+    rules: [
+      {
+        test: /\.(js||jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+    resolve: {
+      extension: ['', '.js', '.jsx']
+    },
+    output: {
+      filename: 'bundle.js',
+      publicPath: '/',
+      path: `${DIST_DIR}`,
+    },
+    devServer: {
+      contentBase: `${DIST_DIR}`
+    }
+  },
+};
