2ac66b138479629aa6f4bc66a72c40882e48a2fa
berhane tendai cole <71781247+berhanecole@users.noreply.github.com>
2021-05-30 12:55:58 -0500

    created hover state function;

--- client/src/components/DrinkTile.jsx
+++ client/src/components/DrinkTile.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Link } from 'react-router-dom'
+
+import useHover from '../../utils/useHover'
 
 
 const DrinkTile = ({ drink }) => {
 
   //* grabbing properties from drink object and reassigning them to less dumb variable names
+  
   const { idDrink: id, strDrink: name, strDrinkThumb: thumbnail } = drink
+  const clickHandler = () => {
+    return (
+      <Link to={`/drink/${id}`}></Link>
+      )
+    }
+
+  //create hover state
+  const [hoverRef, hovering] = useHover();
+
 
   return (
     <div className="col-md-3">
       <div className="drink-tile">
-      <img className="img-fluid drink-thumb border border-secondary" src={thumbnail} />
+      <img 
+        onClick={() => clickHandler()} 
+        ref={hoverRef}
+        className="img-fluid drink-thumb border border-secondary" src={ hovering ? '' : thumbnail} />
       <h3>
         <Link to={`/drink/${id}`}>
         {name}
