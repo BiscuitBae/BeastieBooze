f7161a293b1538deb0a824f5cc9f700f07546e6b
Persis <persis.randolph@gmail.com>
2021-05-31 15:30:08 -0500

    Change login route to use oauth

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 const cookieSession = require('cookie-session');
 const router = require('./routes/index.js')
 require('./passport-setup');
+const { COOKIEKEY } = require('./config')
 
 const PORT = 8080;
 const DIST_DIR = path.resolve(__dirname, '..', 'client/dist');
@@ -15,7 +16,10 @@ app.use(express.static(DIST_DIR))
 
 app.use(cookieSession({
   name: 'beastie-session',
-  keys: ['key1', 'key2']
+  maxAge: 24 * 60 * 60 * 1000, // this will make the cookie last for one day (measured in ms)
+  // keys are how we'd like to encrypy the googleId bfore sending it back to the user
+  // i just used a random string of letters
+  keys: [COOKIEKEY]
 }));
 
 // we might want to move this middleware to another file
@@ -33,13 +37,15 @@ app.use(passport.session());
 
 // example routes for good and bad login attempts
 // app.get('/', (req, res) => res.send('You are not logged in.'));
-// app.get('/failed', (req, res) => res.send('You failed to log in.'))
-// app.get('/good', isLoggedIn, (req, res) => res.send(`Welcome ${req.user.displayName}!`))
+app.get('/failed', (req, res) => res.send('You failed to log in.'))
+app.get('/good', isLoggedIn, (req, res) => res.send(`Welcome ${req.user.displayName}!`))
 
-app.get('/auth/google',
+// callback takes the strategy name (google) and the scope we are trying to retrieve from google (the profile info and email)
+app.get('/login',
   passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-app.get('/auth/google/callback',
+// when a user is redirected back from google it follows this route
+app.get('/googleRedirect',
   // this line needs to redirect to whatever route we want a failed login to direct to
   passport.authenticate('google', { failureRedirect: '/failed' }),
   function(req, res) {
--- server/passport-setup.js
+++ server/passport-setup.js
@@ -3,20 +3,27 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const { CLIENT_ID, CLIENT_SECRET } = require('./config')
 
-passport.serializeUser(function(user, done) {
+// this gives our user a unique identifier to store to the cookie
+passport.serializeUser((user, done) => {
+  // call done method once we get user from the db
   done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(null, user);
-  });
+// takes a session's cookie data and decrypts it to find the matching user
+passport.deserializeUser((id, done) => {
+  // find user by id (THIS HELPER NEEDS TO BE DEFINED IN DB HELPERS)
+  // call done once we've found the user
+  getUser(id)
+    .then(currentUser => {
+      done(null, currentUser[0]);
+    })
 });
 
+// use google strategy that interacts with passport
 passport.use(new GoogleStrategy({
     clientID: CLIENT_ID,
     clientSecret: CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/auth/google/callback"
+    callbackURL: "/googleRedirect"
   },
   // passport callback function
   (accessToken, refreshToken, profile, done) => {
@@ -33,9 +40,27 @@ passport.use(new GoogleStrategy({
     // create a new user object
     const user = { googleId, username, email };
 
-    // use the profile info (mainly the profile id) to check if the user is registered in your db
-    User.findOrCreate({ googleId: profile.id }, function(err, user) {
-      return done(err, user);
-    });
+    // use the profile info (mainly the google id) to check if the user is registered in your db
+
+    // we need to create getUser and createUser helper functions for the db to use this
+    getUser(googleId)
+      .then(currentUser => {
+        currentUser;
+
+        // if the response includes a user obj from our db -> this may need to change depending on the response we expect from our db,
+        // this ex. was using SQL
+        if (currentUser.length) {
+          done(null, currentUser[0]);
+        } else {
+          // if not create a new users in the db
+          createUser(user);
+          getUser(googleId)
+            .then(newUser => {
+              newUser;
+              done(null, newUser[0]);
+            })
+            .catch(err => console.log('error creating new user', err));
+        }
+      })
   }
 ));
