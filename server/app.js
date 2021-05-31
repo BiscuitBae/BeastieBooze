3e6a303f2e972ea54937c701c48519dd172873ae
Persis <persis.randolph@gmail.com>
2021-05-31 11:45:52 -0500

    merging in main

--- server/app.js
+++ server/app.js
@@ -31,9 +31,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // example routes for good and bad login attempts
-app.get('/', (req, res) => res.send('You are not logged in.'));
-app.get('/failed', (req, res) => res.send('You failed to log in.'))
-app.get('/good', isLoggedIn, (req, res) => res.send(`Welcome ${req.user.displayName}!`))
+// app.get('/', (req, res) => res.send('You are not logged in.'));
+// app.get('/failed', (req, res) => res.send('You failed to log in.'))
+// app.get('/good', isLoggedIn, (req, res) => res.send(`Welcome ${req.user.displayName}!`))
 
 app.get('/auth/google',
   passport.authenticate('google', { scope: ['profile', 'email'] }));
