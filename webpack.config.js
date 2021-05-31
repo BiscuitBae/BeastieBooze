d7a785eaa62ab45ca6cbb43482ec99b117f6726c
Bilal <bilalhankins22@gmail.com>
2021-05-31 14:14:43 -0500

    Fixed devtool webpack errors

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
       },
     ],
   },
+ 
     resolve: {
       extensions: ['', '.js', '.jsx']
     },
@@ -22,5 +23,6 @@ module.exports = {
       publicPath: '/',
       path: `${DIST_DIR}`,
     },
+    devtool: "source-map"
   };
 
