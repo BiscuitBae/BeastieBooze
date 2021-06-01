a1ed282fcd9324c99edd6148563e19245f3aa090
berhane tendai cole <71781247+berhanecole@users.noreply.github.com>
2021-06-01 10:06:03 -0500

    updated information in index.js

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Can swap Hash Router for Browser Router
+
 import { HashRouter as Router } from 'react-router-dom'
 import App from './components/App';
 // import 'bootstrap/dist/css/bootstrap.min.css';
