12996bff127d777d3043555a8b73ffbcbf13b493
Bilal <bilalhankins22@gmail.com>
2021-06-03 16:16:47 -0500

    Initial route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,17 @@
 const { Router } = require("express");
 const dotenv = require('dotenv');
 const { getUser, createUser } = require('../database/helpers');
+const { User } = require('../database/Models');
+
 dotenv.config();
 
 const axios = require('axios');
 
 const usersRouter = Router();
+
+usersRouter.get('/', (req, res) => {
+
+});
 
 usersRouter.post('/', (req, res) => {
   const { googleId, username } = req.body
@@ -13,9 +19,14 @@ usersRouter.post('/', (req, res) => {
 
   if(existingUser){
     console.log(existingUser)
-    res.status(201).send(existingUser)
+    res.status(201).send(existingUser);
+  } else if (!existingUser) {
+    console.log('newUser')
+    res.status(201).send(createUser(req.body));
+  } else {
+    console.log('not found')
+    res.sendStatus(404);
   }
-
 });
 
 
