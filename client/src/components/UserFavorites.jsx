6361af7433afac62d8e1ab7a41643cc50af29e67
berhane tendai cole <71781247+berhanecole@users.noreply.github.com>
2021-06-04 18:31:52 -0500

    favorites are added to the database;

--- client/src/components/UserFavorites.jsx
+++ client/src/components/UserFavorites.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 
 const UserFavorites = ({ favorites }) => {
 
+  console.log(favorites)
+
+//db changes related to favorites
+  const whichDb = (e) => {
+    let name = e.strDrink ? e.strDrink : e.drinkName
+    return name
+  }
+
   return (
     <ul>
-      {favorites.map(e => {
+      {favorites.map((e, i) => {
         return (
-          <li key={e.id}>{e.name}</li>
+          <li key={i}>{whichDb(e)}</li>
         )
       })}
     </ul>
