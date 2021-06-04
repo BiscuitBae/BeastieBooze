d3a1cffa6094e23d66842f5aa9fa8efdaf002a41
Persis <persis.randolph@gmail.com>
2021-06-04 15:17:10 -0500

    uninstall extra dependency

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -2,8 +2,6 @@ import React, { useEffect, useContext } from 'react';
 import DrinkTile from '../components/DrinkTile';
 import {BoozeContext} from '../boozeContext';
 import {UserContext} from '../userContext';
-import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
 
 const Feed = () => {
 // pull drinksFeed (current assortment of drinks) and random10 function from BoozeContext
