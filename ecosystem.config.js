09773bbf4aeddef089cb673afff2c7ebdf963adc
Persis <persis.randolph@gmail.com>
2021-06-04 15:55:42 -0500

    fixed dotenv path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,6 @@
-const dotenv = require('dotenv');
+const path = require('path');
+const dotenv = require('dotenv').config({ path: path.join(__dirname, '.env') });
+
 dotenv.config();
 
 module.exports = {
