5814b3881780c14065852cda48c357af0c1d5934
David Palaciose <dboxing22@gmail.com>
2021-06-07 17:15:26 -0500

    fixed mongo issues

--- server/database/Models.js
+++ server/database/Models.js
@@ -3,11 +3,13 @@ dotenv.config();
 
 const mongoose = require('mongoose');
 
-const DATABASE = 'BeastieBooze';
+// const DATABASE = 'BeastieBooze';
+const DATABASE = 'mongodb://localhost/beastie-booze';
 // for dev - uncomment the next line and comment out line 10
 // const dbLocation = `mongodb://localhost:27017/${DATABASE}`;
 // for prod
-const dbLocation = `${process.env.ATLAS_URL}${DATABASE}`;
+// const dbLocation = `${process.env.ATLAS_URL}${DATABASE}`;
+const dbLocation = `${DATABASE}`;
 // const dbLocation = process.env.ATLAS_URL;
 
 mongoose.connect(dbLocation, {useNewUrlParser: true, useUnifiedTopology: true})
