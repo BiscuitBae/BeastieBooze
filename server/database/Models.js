6e6e7b6f4859ef8b386a3e141874c090edbb969b
Jon Hourcade <jonhourcade@Jons-Mac-mini.local>
2021-06-07 17:14:42 -0500

    added schema

--- server/database/Models.js
+++ server/database/Models.js
@@ -2,18 +2,21 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const mongoose = require('mongoose');
+const { number } = require('prop-types');
 
 const DATABASE = 'BeastieBooze';
 // for dev - uncomment the next line and comment out line 10
-// const dbLocation = `mongodb://localhost:27017/${DATABASE}`;
+const dbLocation = `mongodb://localhost:27017/${DATABASE}`;
 // for prod
-const dbLocation = `${process.env.ATLAS_URL}${DATABASE}`;
+// const dbLocation = `${process.env.ATLAS_URL}${DATABASE}`;
 // const dbLocation = process.env.ATLAS_URL;
 
-mongoose.connect(dbLocation, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose
+  .connect(dbLocation, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log(`sucessfully connected! ${DATABASE}`);
-  }).catch(err => console.error('Failed to connect to database', err));
+  })
+  .catch((err) => console.error('Failed to connect to database', err));
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -22,7 +25,8 @@ const UserSchema = new mongoose.Schema({
   googleId: String, // not sure if this will a string or a number, need to check once we can get data from google
   username: String,
   favorites: [],
-  creations: []
+  creations: [],
+  businessId: mongoose.Schema.Types.ObjectId,
 });
 
 const DrinkSchema = new mongoose.Schema({
@@ -30,21 +34,46 @@ const DrinkSchema = new mongoose.Schema({
   instructions: String,
   ingredients: {},
   alcoholic: Boolean,
-  createdBy: String
+  createdBy: String,
+  soldAt: [{ type: mongoose.Schema.Types.ObjectId }],
   //add a createdBy to the drinkSchema to link to Users once created
+});
+
+const BusinessSchema = new mongoose.Schema({
+  name: String,
+  contactInformation: {
+    address: String,
+    phone: String,
+    email: String,
+  },
+  details: {
+    hoursOfOperation: String,
+    description: String,
+  },
+  menu: [{ type: mongoose.Schema.Types.ObjectId }], // drink docs
+  transactions: [{ type: mongoose.Schema.Types.ObjectId }], // transaction docs
+});
+
+const TransactionSchema = new mongoose.Schema({
+  drinkId: mongoose.Schema.Types.ObjectId,
+  businessId: mongoose.Schema.Types.ObjectId,
+  quantity: Number,
+  date: Date,
 });
 
 const User = mongoose.model('User', UserSchema);
 const Drink = mongoose.model('Drink', DrinkSchema);
+const Business = mongoose.model('Business', BusinessSchema);
+const Transaction = mongoose.model('Transaction', TransactionSchema);
 
 const addDrink = async (drink) => {
-  const { drinkName: name, instructions, ingredients, alcoholic } = drink
+  const { drinkName: name, instructions, ingredients, alcoholic } = drink;
   const newDrink = new Drink({
     name,
     instructions,
     ingredients,
-    alcoholic
-  })
+    alcoholic,
+  });
   await newDrink.save();
 };
 
@@ -55,6 +84,8 @@ const getDrinks = async () => {
 module.exports = {
   User,
   Drink,
+  Business,
+  Transaction,
   addDrink,
   getDrinks,
 };
